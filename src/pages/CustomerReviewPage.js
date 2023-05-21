2c512334e8be6715906ebcb78c956714648a485a
georgeowusu <gkojunior@gmail.com>
2023-05-21 09:13:10 -0400

    first commit

---
+++ src/pages/CustomerReviewPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CustomerReviewPage = () => {
+    return <h4>Customer Review Page</h4>
+}
+  
+export default CustomerReviewPage
